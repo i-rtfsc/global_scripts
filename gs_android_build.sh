aff859829cbb63609a013c71a92467c6840b9816
Solo <anqi.huang@outlook.com>
2022-06-30 20:50:58 +0800

    Update android build
    - Update android build
    
    Signed-off-by: Solo <anqi.huang@outlook.com>

--- gs_android_build.sh
+++ gs_android_build.sh
@@ -20,91 +20,114 @@
 # 可根据需求自行更改
 case `uname -s` in
     Darwin)
-        _gs_local_build_thread=$(sysctl -n hw.logicalcpu)
+        _GS_BUILD_THREAD=$(sysctl -n hw.logicalcpu)
         ;;
     *)
-        _gs_local_build_thread=$(nproc)
+        _GS_BUILD_THREAD=$(nproc)
         ;;
 esac
 
 # target product
 # 这么设置就要求在终端先lunch一次
 # 如果没有lunch就是用默认target
-_gs_local_build_target=${TARGET_PRODUCT}
+_GS_BUILD_TARGET=${TARGET_PRODUCT}
 
 # 设置的默认target（lineage_lemonadep-userdebug）
-_gs_local_build_target_default="lineage_lemonadep-userdebug"
+_GS_BUILD_TARGET_DEFAULT="lineage_lemonadep-userdebug"
 
 # 机器人地址
-_gs_bot="93c6a139-2a53-44ec-9711-850dd3a1e6f4"
+_GS_BOT="93c6a139-2a53-44ec-9711-850dd3a1e6f4"
+
+# 使用ccache
+_GS_CCACHE=false
 
 function _gs_android_build_with_ccache() {
-    export USE_CCACHE=1
-    export CCACHE_EXEC=/usr/bin/ccache
-    #set ccache dir
-    export CCACHE_DIR=$HOME/ext-data/.ccache
-    ccache -M 50G
+    if $_GS_CCACHE ; then
+        export USE_CCACHE=1
+        export CCACHE_EXEC=/usr/bin/ccache
+        #set ccache dir
+        export CCACHE_DIR=$HOME/ext-data/.ccache
+        ccache -M 50G
+    fi
 }
 
 function _gs_notify_bot() {
-    if [ -z ${_gs_bot} ]; then
+    if [ -z ${_GS_BOT} ]; then
         return 0
     fi
-    curl -X POST -H "Content-Type: application/json" -d '{"msg_type":"text","content":{"text":"build finish"}}' https://open.feishu.cn/open-apis/bot/v2/hook/${_gs_bot}
+    curl -X POST -H "Content-Type: application/json" -d '{"msg_type":"text","content":{"text":"build finish"}}' https://open.feishu.cn/open-apis/bot/v2/hook/${_GS_BOT}
+}
+
+function _gs_print_info() {
+  # Android平台的版本号
+  echo "Android platform version = $PLATFORM_VERSION"
+  echo "build product = $TARGET_PRODUCT"
+  echo "build variant = $TARGET_BUILD_VARIANT"
+  echo "build type = $TARGET_BUILD_TYPE"
+  # 表示编译目标的CPU架构
+  echo "build arch = $TARGET_ARCH"
+  # 表示编译目标的CPU架构版本
+  echo "build arch variant = $TARGET_ARCH_VARIANT"
+  # 表示编译目标的CPU代号
+  echo "build cpu variant = $TARGET_CPU_VARIANT"
+  echo "BUILD_ID = $BUILD_ID"
+  echo "OUT_DIR = $OUT_DIR"
 }
 
 function _gs_android_build_lunch() {
     TOP=`pwd`
-    local building_log_dir=$TOP/out/build_log
+    local gs_build_log_dir=$TOP/out/build_log
     # check if the building log dir exists
-    if [ ! -d ${building_log_dir} ]; then
-        mkdir -p ${building_log_dir}
+    if [ ! -d ${gs_build_log_dir} ]; then
+        mkdir -p ${gs_build_log_dir}
     fi
-    export _GS_BUILD_LOG_DIR=${building_log_dir}
+    export _GS_BUILD_LOG_DIR=${gs_build_log_dir}
 
-    local LOCAL_TARGET_PRODUCT=${_gs_local_build_target}
+    local gs_target_product=${_GS_BUILD_TARGET}
     if [ -z ${LOCAL_TARGET_PRODUCT} ]; then
-        LOCAL_TARGET_PRODUCT=$1
+        gs_target_product=$1
     fi
 
-    if [ -z ${LOCAL_TARGET_PRODUCT} ]; then
-        LOCAL_TARGET_PRODUCT=${_gs_local_build_target_default}
+    if [ -z ${target_product} ]; then
+        gs_target_product=${_GS_BUILD_TARGET_DEFAULT}
     fi
 
     source build/envsetup.sh
-    lunch ${LOCAL_TARGET_PRODUCT}
+    lunch ${gs_target_product}
+
+    _gs_print_info
 }
 
 function gs_android_build() {
-    #_gs_android_build_with_ccache
+    _gs_android_build_with_ccache
 
     # lunch target
     _gs_android_build_lunch
 
     # log file
-    local building_time=$(date "+%Y-%m-%d-%H-%M-%S")
-    local building_log=${_GS_BUILD_LOG_DIR}/build_full_${building_time}.log
+    local build_time=$(date "+%Y-%m-%d-%H-%M-%S")
+    local build_log=${_GS_BUILD_LOG_DIR}/build_full_${build_time}.log
 
     # full build
-    m -j ${_gs_local_build_thread} 2>&1 | tee ${building_log}
+    m -j ${_GS_BUILD_THREAD} 2>&1 | tee ${build_log}
     _gs_notify_bot
 }
 
 function gs_android_build_ota() {
-    #_gs_android_build_with_ccache
+    _gs_android_build_with_ccache
 
     # lunch target
     _gs_android_build_lunch
 
     # log file
-    local building_time=$(date "+%Y-%m-%d-%H-%M-%S")
-    local building_log=${_GS_BUILD_LOG_DIR}/build_full_${building_time}.log
-    local building_ota_log=${_GS_BUILD_LOG_DIR}/build_ota_${building_time}.log
+    local build_time=$(date "+%Y-%m-%d-%H-%M-%S")
+    local build_log=${_GS_BUILD_LOG_DIR}/build_full_${build_time}.log
+    local build_ota_log=${_GS_BUILD_LOG_DIR}/build_ota_${build_time}.log
 
     # full build
-    m -j ${_gs_local_build_thread} 2>&1 | tee ${building_log}
+    m -j ${_GS_BUILD_THREAD} 2>&1 | tee ${build_log}
     # make ota
-    make otapackage -j ${_gs_local_build_thread} 2>&1 | tee ${building_ota_log}
+    make otapackage -j ${_GS_BUILD_THREAD} 2>&1 | tee ${build_ota_log}
     _gs_notify_bot
 }
 
@@ -117,11 +140,11 @@ function _gs_android_build_system() {
     local goals=$1
 
     # log file
-    local building_time=$(date "+%Y-%m-%d-%H-%M-%S")
-    local building_log=${_GS_BUILD_LOG_DIR}/build_${goals}_${building_time}.log
+    local build_time=$(date "+%Y-%m-%d-%H-%M-%S")
+    local build_log=${_GS_BUILD_LOG_DIR}/build_${goals}_${build_time}.log
 
     # build
-    m -j ${_gs_local_build_thread} ${goals} 2>&1 | tee ${building_log}
+    m -j ${_GS_BUILD_THREAD} ${goals} 2>&1 | tee ${build_log}
     _gs_notify_bot
 }
 
@@ -153,18 +176,18 @@ function gs_lineage_build() {
     breakfast ${LOCAL_TARGET_PRODUCT}
 
     TOP=`pwd`
-    local building_log_dir=$TOP/out/build_log
+    local build_log_dir=$TOP/out/build_log
     # check if the building log dir exists
-    if [ ! -d ${building_log_dir} ]; then
-        mkdir -p ${building_log_dir}
+    if [ ! -d ${build_log_dir} ]; then
+        mkdir -p ${build_log_dir}
     fi
 
     # log file
-    local building_time=$(date "+%Y-%m-%d-%H-%M-%S")
-    local building_log=${building_log_dir}/build_lineage_${building_time}.log
+    local build_time=$(date "+%Y-%m-%d-%H-%M-%S")
+    local build_log=${build_log_dir}/build_lineage_${build_time}.log
 
     # lineage build
-    brunch ${LOCAL_TARGET_PRODUCT} 2>&1 | tee ${building_log}
+    brunch ${LOCAL_TARGET_PRODUCT} 2>&1 | tee ${build_log}
     _gs_notify_bot
 }
 
@@ -173,18 +196,18 @@ function _gs_modules() {
                   "framework"
                   "framework-minus-apex"
                   "services"
+                  "selinux_policy"
                   "surfaceflinger"
                   "update_engine"
                   "android.hardware.power-service"
-                  "J007Service"
-                  "jos-framework"
-                  "com.journeyOS.J007engine.hidl@1.0-service"
-                  "watermark"
-                  "AiService"
-                  "bx-framework"
-                  "UMS"
                   "SystemUI"
                   "Settings"
+                  "com.journeyOS.J007engine.hidl@1.0-service"
+                  "com.journeyOS.J007engine.hidl@1.0"
+                  "J007Service"
+                  "watermark"
+                  "u-framework"
+                  "u-services"
                   )
 #    echo $modules
     for item in ${modules[@]}; do
@@ -237,7 +260,7 @@ function _gs_show_and_choose_combo() {
 }
 
 function gs_android_build_ninja_clean() {
-    time prebuilts/build-tools/linux-x86/bin/ninja -j ${_gs_local_build_thread} -f out/combined-${TARGET_PRODUCT}.ninja -t clean
+    time prebuilts/build-tools/linux-x86/bin/ninja -j ${_GS_BUILD_THREAD} -f out/combined-${TARGET_PRODUCT}.ninja -t clean
 }
 
 function gs_android_build_ninja() {
@@ -252,12 +275,12 @@ function gs_android_build_ninja() {
     selection=${_GS_BUILD_COMBO}
 
     # log file
-    local building_time=$(date "+%Y-%m-%d-%H-%M-%S")
-    building_log=${_GS_BUILD_LOG_DIR}/ninja_build_${selection}_${building_time}.log
-    echo "selection = "${selection} ", building log =" ${building_log}
+    local build_time=$(date "+%Y-%m-%d-%H-%M-%S")
+    build_log=${_GS_BUILD_LOG_DIR}/ninja_build_${selection}_${build_time}.log
+    echo "selection = "${selection} ", building log =" ${build_log}
 
     # ninja build
-    time prebuilts/build-tools/linux-x86/bin/ninja -j ${_gs_local_build_thread} -f out/combined-${TARGET_PRODUCT}.ninja ${selection} | tee ${building_log}
+    time prebuilts/build-tools/linux-x86/bin/ninja -j ${_GS_BUILD_THREAD} -f out/combined-${TARGET_PRODUCT}.ninja ${selection} | tee ${build_log}
 }
 
 function gs_android_build_make() {
@@ -272,10 +295,10 @@ function gs_android_build_make() {
     selection=${_GS_BUILD_COMBO}
 
     # log file
-    local building_time=$(date "+%Y-%m-%d-%H-%M-%S")
-    building_log=${_GS_BUILD_LOG_DIR}/make_build_${selection}_${building_time}.log
-    echo "selection = "${selection} ", building log =" ${building_log}
+    local build_time=$(date "+%Y-%m-%d-%H-%M-%S")
+    build_log=${_GS_BUILD_LOG_DIR}/make_build_${selection}_${build_time}.log
+    echo "selection = "${selection} ", building log =" ${build_log}
 
     # make build
-    make ${selection} -j ${_gs_local_build_thread} | tee ${building_log}
+    make ${selection} -j ${_GS_BUILD_THREAD} | tee ${build_log}
 }
